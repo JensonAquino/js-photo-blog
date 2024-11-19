8594936c0669d1af52850878eb9bc666a6ea84d0
JensonAquino <jensonaquino2622@gmail.com>
2024-11-19 23:58:13 +0100

    feat: paggina responsive fatta

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const generatePhoto = () => {
     let photInner = "";
     photos.forEach(curPhoto => {
         photInner += `
-        <div class="col-3 bg-light p-2 card mt-4">
+        <div class="col-lg-3 col-md-5 col-sm-12 bg-light p-2 card mt-4">
             <img class="photo" src="${curPhoto.url}" alt="">
             <img class="dott" src="./img/pin.svg" alt="">
             <section>${curPhoto.title}</section>
